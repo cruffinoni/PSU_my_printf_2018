1c8ea1adf36682977ffb9a72388181e4aa1925c6
Clément RUFFINONI <cruffinoni@linux.home>
2018-11-18 23:34:23 +0100

    Precision with pointer wasn't handled correctly

--- lib/my/my_printf/src/type/pointer.c
+++ lib/my/my_printf/src/type/pointer.c
@@ -17,14 +17,27 @@
 #include "../utils/print/print_uint.h"
 #include "../utils/flags.h"
 
+static int print_extra_space(t_local_spe specifier_infos, int nb_len, int
+    count)
+{
+    if ((specifier_infos.flags_extra & EXTRAF_MINUS) == EXTRAF_MINUS)
+        if (nb_len < specifier_infos.precision && specifier_infos.precision >
+            count)
+            count += print_char_ite(specifier_infos.precision - count, ' ');
+    return (count);
+}
+
 int type_pointer(va_list args, t_local_spe specifier_infos)
 {
     void *var = va_arg(args, void *);
     int count = 2;
-    int nb_len = uint_len((intptr_t) var) - 1;
+    int nb_len = 14;
 
-    if ((specifier_infos.flags_extra & EXTRAF_PLUS) != EXTRAF_PLUS)
+    if ((specifier_infos.flags_extra & EXTRAF_PLUS) != EXTRAF_PLUS) {
+        if ((specifier_infos.flags_extra & EXTRAF_SPACE) == EXTRAF_SPACE)
+            count += my_putchar(' ');
         count += print_extraf(specifier_infos, 0, 0);
+    }
     else
         specifier_infos.precision--;
     count += print_precision(specifier_infos, nb_len);
@@ -32,9 +45,6 @@ int type_pointer(va_list args, t_local_spe specifier_infos)
         count += print_extraf(specifier_infos, 0, 0);
     write(1, "0x", 2);
     uint_print_nbr((intptr_t) var, &count, 0, 16);
-    if ((specifier_infos.flags_extra & EXTRAF_MINUS) == EXTRAF_MINUS)
-        if (nb_len < specifier_infos.precision && specifier_infos.precision >
-            count)
-            count += print_char_ite(specifier_infos.precision - count, ' ');
+    print_extra_space(specifier_infos, nb_len, count);
     return (count);
 }
