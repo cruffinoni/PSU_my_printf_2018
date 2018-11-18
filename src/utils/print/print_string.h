444cbdf5e77acac13eade28298357ac5e521daf9
Clément RUFFINONI <cruffinoni@linux.home>
2018-11-18 16:17:20 +0100

    Missing header and avoided the project to compile

--- src/utils/print/print_string.h
+++ src/utils/print/print_string.h
@@ -8,6 +8,8 @@
 #ifndef HEADER_SRC_PRINTSTR
 #define HEADER_SRC_PRINTSTR
 
+#include "../../my_printf.h"
+
 int print_string(char *string, int *count, int escape_char, t_local_spe spe);
 int print_char_ite(int count, char c);
 void put_char_base(int number, int upper_case);
